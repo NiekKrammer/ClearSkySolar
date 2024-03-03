6170adaf911a099c1c539a57ec2464c22e740807
NiekKrammer <niekkrammer@gmail.com>
2024-03-03 16:08:53 +0100

    gelijk inloggen na registreren. Nieuwe syles.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,21 +10,36 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class RegistrationController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
+    private UserPasswordHasherInterface $userPasswordHasher;
+    private TokenStorageInterface $tokenStorage;
+    private EventDispatcherInterface $eventDispatcher;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(
+        EntityManagerInterface      $entityManager,
+        UserPasswordHasherInterface $userPasswordHasher,
+        TokenStorageInterface       $tokenStorage,
+        EventDispatcherInterface    $eventDispatcher
+    )
     {
         $this->entityManager = $entityManager;
+        $this->userPasswordHasher = $userPasswordHasher;
+        $this->tokenStorage = $tokenStorage;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     #[Route('/registration', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
+    public function register(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -39,10 +54,10 @@ class RegistrationController extends AbstractController
                 return $this->render('registration.html.twig', [
                     'registrationForm' => $form->createView(),
                 ]);
-            }   
+            }
 
             $user->setPassword(
-                $userPasswordHasher->hashPassword(
+                $this->userPasswordHasher->hashPassword(
                     $user,
                     $password
                 )
@@ -53,10 +68,18 @@ class RegistrationController extends AbstractController
 
             $this->entityManager->persist($user);
             $this->entityManager->flush();
-            $this->addFlash('registration_success', 'Je registratie is succesvol voltooid! je kunt nu inloggen.');
+            $this->addFlash('registration_success', 'Je registratie is succesvol voltooid!');
             $this->sendEmail($user);
 
-            return $this->redirectToRoute('app_login');
+            // Handmatig inloggen van de gebruiker na registratie
+            $token = new UsernamePasswordToken($user, 'main', $user->getRoles());
+            $this->tokenStorage->setToken($token);
+
+            // Dispatch de login event
+            $event = new InteractiveLoginEvent($request, $token);
+            $this->eventDispatcher->dispatch($event);
+
+            return $this->redirectToRoute('app_default');
         }
 
         return $this->render('registration.html.twig', [
@@ -75,9 +98,9 @@ class RegistrationController extends AbstractController
         $phpmailer->Username = 'api';
         $phpmailer->Password = '67a481dd00090e37922f82e2d6f458d4';
 
-        $phpmailer->setFrom('qeuqen@niekkrammer.nl');
+        $phpmailer->setFrom('clearskysolar@niekkrammer.nl');
         $phpmailer->addAddress($user->getEmail(), $user->getUsername());
-        $phpmailer->Subject = 'Registratiebevestiging qeuqen';
+        $phpmailer->Subject = 'Registratiebevestiging ClearSkySolar';
         $phpmailer->isHTML(true);
         $phpmailer->Body = $this->renderView('emails/registration.html.twig', [
             'username' => $user->getUsername(),
@@ -89,4 +112,5 @@ class RegistrationController extends AbstractController
             dump($e->getMessage());
         }
     }
+
 }
