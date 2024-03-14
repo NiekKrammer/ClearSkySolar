96cc1dd4a08b0ad55559b5b449069ff7c36fdbb1
NiekKrammer <niekkrammer@gmail.com>
2024-03-14 20:11:52 +0100

    new styles

--- migrations/Version20240314190924.php
+++ migrations/Version20240314190924.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240313085951 extends AbstractMigration
+final class Version20240314190924 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240313085951 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON user (username)');
+        $this->addSql('ALTER TABLE `order` ADD test VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX UNIQ_8D93D649F85E0677 ON user');
+        $this->addSql('ALTER TABLE `order` DROP test');
     }
 }
